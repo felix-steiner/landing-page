c3549f9421a83e69a1db754681bcce51a7d30947
Felix Steiner <felix.steiner7@gmail.com>
2021-06-04 22:07:45 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './screens/home/home.component';
+import { LegalComponent } from './screens/legal/legal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LegalComponent
   ],
   imports: [
     BrowserModule,
