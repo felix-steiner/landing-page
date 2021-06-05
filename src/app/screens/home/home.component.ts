5289bf612eaae4b8ff63c44f5178456edf93fc12
Felix Steiner <felix.steiner7@gmail.com>
2021-06-05 10:22:19 +0200

    Adjusted responsivness

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  isMobileMenuShown = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  showMobileMenu(show: boolean) {
+   this.isMobileMenuShown = show;
+   console.log('menu is toggeled');
+  }
 }
